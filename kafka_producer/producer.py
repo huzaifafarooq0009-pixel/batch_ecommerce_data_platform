ae300791e7bda0f2283a53b2f750f0a874691852
Huzaifa Farooq <huzaifafarooq0009@gmail.com>
2026-06-21 19:27:41 +0500

    Kafka system introduced!

--- kafka_producer/producer.py
+++ kafka_producer/producer.py
@@ -1,27 +1,20 @@
 from kafka import KafkaProducer
 import json
-import time
 
-# Connect to Kafka server
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-print("Kafka Producer Started")
+order = {
+    "order_id": 1,
+    "customer_id": 10,
+    "product_id": 5,
+    "quantity": 2
+}
 
-for i in range(10):
-
-    order = {
-        "order_id": i,
-        "customer_id": 100 + i,
-        "amount": 200 + i
-    }
-
-    producer.send("orders_topic", order)
-
-    print("Sent:", order)
-
-    time.sleep(1)
+producer.send("orders_topic", order)
 
 producer.flush()
+
+print("Order sent successfully!")
